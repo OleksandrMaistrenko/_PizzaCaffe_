9b62af0aedcb9627e57f9a014421af7282a84d4c
Oleksandr Maistrenko <oleksandrmaistrenko100@gmail.com>
2024-04-26 17:34:59 +0200

    modal all

--- client/src/components/cart/emptycart/emptyCart.js
+++ client/src/components/cart/emptycart/emptyCart.js
@@ -3,7 +3,15 @@ import React from "react";
 import "./emptyCart.css";
 
 const emptyCart = () => {
-  return <div className="containerCart"></div>;
+  return (
+    <div className="container">
+      <h1>Your Cart</h1>
+      <p className="empty-cart">Your cart is empty</p>
+      <Link to="/orders" className="back-btn">
+        Go to orders
+      </Link>
+    </div>
+  );
 };
 
 export default emptyCart;
--- client/src/components/orderForm/MyForm.js
+++ client/src/components/orderForm/MyForm.js
@@ -10,6 +10,9 @@ const MyForm = ({ setShowModal, showModal }) => {
     state: "",
   });
 
+  const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [showThankYouModal, setShowThankYouModal] = useState(false);
+
   const [errors, setErrors] = useState({
     firstName: "",
     lastName: "",
@@ -38,10 +41,12 @@ const MyForm = ({ setShowModal, showModal }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    console.log("Form submitted");
+    console.log(formData);
 
     const validationErrors = validateForm(formData);
     if (Object.keys(validationErrors).length === 0) {
-      setShowModal(false);
+      setShowConfirmationModal(true);
     } else {
       setErrors(validationErrors);
     }
@@ -69,138 +74,214 @@ const MyForm = ({ setShowModal, showModal }) => {
     return errors;
   };
 
+  const handlePayment = () => {
+    // Place your payment logic here
+    setShowConfirmationModal(false);
+    setShowThankYouModal(true);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      city: "",
+      state: "",
+    });
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setShowConfirmationModal(false);
+    setShowThankYouModal(false);
+    setFormData({
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      city: "",
+      state: "",
+    });
+  };
+
   return (
-    <Modal show={showModal} onHide={() => setShowModal(false)}>
-      <Modal.Header closeButton>
-        <Modal.Title>Your Data</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div className="row justify-content-center">
-          <div className="col-md-8">
-            <form
-              id=""
-              className="row g-3"
-              ref={formRef}
-              onSubmit={handleSubmit}
-            >
-              <div className="col-md-12">
-                <label htmlFor="validationServer01" className="form-label">
-                  First name
-                </label>
-                <input
-                  type="text"
-                  className="form-control  custom-form-control"
-                  id="validationServer01"
-                  value={formData.firstName}
-                  onChange={handleChange}
-                  name="firstName"
-                  required
-                />
-                {errors.firstName && (
-                  <div className="invalid-feedback">{errors.firstName}</div>
-                )}
-              </div>
-              <div className="col-md-12">
-                <label htmlFor="validationServer02" className="form-label">
-                  Last name
-                </label>
-                <input
-                  type="text"
-                  className="form-control  custom-form-control"
-                  id="validationServer02"
-                  value={formData.lastName}
-                  onChange={handleChange}
-                  name="lastName"
-                  required
-                />
-                <div className="valid-feedback">Looks good!</div>
-              </div>
-              <div className="col-md-12">
-                <label
-                  htmlFor="validationServerUsername"
-                  className="form-label"
-                >
-                  Phone Number
-                </label>
-                <div className="input-group  custom-form-control">
+    <>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Your Data</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+              <form
+                id="myForm"
+                className="row g-3"
+                ref={formRef}
+                onSubmit={submitForm}
+              >
+                <div className="col-md-12">
+                  <label htmlFor="validationServer01" className="form-label">
+                    First name
+                  </label>
                   <input
-                    type="phone"
-                    className="form-control "
-                    id="validationServerUsername"
-                    value={formData.phoneNumber}
-                    aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback"
+                    type="text"
+                    className="form-control  custom-form-control"
+                    id="validationServer01"
+                    value={formData.firstName}
                     onChange={handleChange}
-                    name="phoneNumber"
+                    name="firstName"
+                    required
+                  />
+                  {errors.firstName && (
+                    <div className="invalid-feedback">{errors.firstName}</div>
+                  )}
+                </div>
+                <div className="col-md-12">
+                  <label htmlFor="validationServer02" className="form-label">
+                    Last name
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control  custom-form-control"
+                    id="validationServer02"
+                    value={formData.lastName}
+                    onChange={handleChange}
+                    name="lastName"
+                    required
+                  />
+                  <div className="valid-feedback">Looks good!</div>
+                </div>
+                <div className="col-md-12">
+                  <label
+                    htmlFor="validationServerUsername"
+                    className="form-label"
+                  >
+                    Phone Number
+                  </label>
+                  <div className="input-group  custom-form-control">
+                    <input
+                      type="phone"
+                      className="form-control "
+                      id="validationServerUsername"
+                      value={formData.phoneNumber}
+                      aria-describedby="inputGroupPrepend3 validationServerUsernameFeedback"
+                      onChange={handleChange}
+                      name="phoneNumber"
+                      required
+                    />
+                    <div
+                      id="validationServerUsernameFeedback"
+                      className="invalid-feedback"
+                    >
+                      Please enter your a number.
+                    </div>
+                  </div>
+                </div>
+                <div className="col-md-12">
+                  <label htmlFor="validationServer03" className="form-label">
+                    City
+                  </label>
+                  <input
+                    type="text"
+                    className="form-control  custom-form-control"
+                    id="validationServer03"
+                    aria-describedby="validationServer03Feedback"
+                    onChange={handleChange}
+                    value={formData.city}
+                    name="city"
                     required
                   />
                   <div
-                    id="validationServerUsernameFeedback"
+                    id="validationServer03Feedback"
                     className="invalid-feedback"
                   >
-                    Please enter your a number.
+                    Please provide a valid city.
                   </div>
                 </div>
-              </div>
-              <div className="col-md-12">
-                <label htmlFor="validationServer03" className="form-label">
-                  City
-                </label>
-                <input
-                  type="text"
-                  className="form-control  custom-form-control"
-                  id="validationServer03"
-                  aria-describedby="validationServer03Feedback"
-                  onChange={handleChange}
-                  value={formData.city}
-                  name="city"
-                  required
-                />
-                <div
-                  id="validationServer03Feedback"
-                  className="invalid-feedback"
-                >
-                  Please provide a valid city.
+                <div className="col-md-12">
+                  <label htmlFor="validationServer04" className="form-label">
+                    State
+                  </label>
+                  <select
+                    className="form-select  custom-form-control"
+                    id="validationServer04"
+                    aria-describedby="validationServer04Feedback"
+                    onChange={handleChange}
+                    value={formData.state}
+                    name="state"
+                    required
+                  >
+                    <option disabled value="">
+                      Choose...
+                    </option>
+                    <option>Delivery</option>
+                    <option>To Go</option>
+                  </select>
+                  <div
+                    id="validationServer04Feedback"
+                    className="invalid-feedback"
+                  >
+                    Please select a valid state.
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-12">
-                <label htmlFor="validationServer04" className="form-label">
-                  State
-                </label>
-                <select
-                  className="form-select  custom-form-control"
-                  id="validationServer04"
-                  aria-describedby="validationServer04Feedback"
-                  onChange={handleChange}
-                  value={formData.state}
-                  name="state"
-                  required
-                >
-                  <option disabled value="">
-                    Choose...
-                  </option>
-                  <option>Delivery</option>
-                  <option>To Go</option>
-                </select>
-                <div
-                  id="validationServer04Feedback"
-                  className="invalid-feedback"
-                >
-                  Please select a valid state.
-                </div>
-              </div>
-            </form>
+              </form>
+            </div>
           </div>
-        </div>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={() => setShowModal(false)}>
-          Close
-        </Button>
-        <Button variant="primary" onClick={() => submitForm()}>
-          Save
-        </Button>
-      </Modal.Footer>
-    </Modal>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Close
+          </Button>
+          <Button
+            variant="primary"
+            type="submit"
+            form="myForm"
+            onClick={handleSubmit}
+          >
+            Save
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={showConfirmationModal}
+        onHide={() => setShowConfirmationModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Confirmation</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Your data</p>
+          <p>First Name: {formData.firstName}</p>
+          <p>Last Name: {formData.lastName}</p>
+          <p>Phone Number: {formData.phoneNumber}</p>
+          <p>City: {formData.city}</p>
+          <p>State: {formData.state}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => setShowConfirmationModal(false)}
+          >
+            Close
+          </Button>
+          <Button variant="primary" onClick={() => handlePayment()}>
+            Pay
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Modal
+        show={showThankYouModal}
+        onHide={() => setShowThankYouModal(false)}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Thank You!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Thank you for Order</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setShowThankYouModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
